254974442a561c246387b076a2110537c006dcf7
arunpravin125@gmail.com <arunpravin125@gmail.com>
2024-09-12 17:48:05 +0530

    socket bugs fix

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({children})=>{
     const {authUser}=useAuthContext()
     useEffect(()=>{
   if(authUser){
-    const socket = io("http://localhost:4500",{
+    const socket = io("https://chat-youtube-mern-1.onrender.com",{
         query:{
             userId:authUser._id
         }
